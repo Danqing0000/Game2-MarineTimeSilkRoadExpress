a5c4b62f6cab787769995cd6e6b3413add72e9e8
Danqing0000 <danqingliu326@163.com>
2023-11-28 12:58:29 +0000

    11.28
    Add new model

--- Assets/_Script/GameManager.cs
+++ Assets/_Script/GameManager.cs
@@ -8,10 +8,10 @@ public class GameManager : MonoBehaviour
     public List<GameObject> RandomCharacter;
     public List<GameObject> CRObjectList; //contain all the antiques
     //public List<List<string>> CRContentList;
-    public List<string> Cube; //contain all the requirements(reception scene), reference(warehouse scene), tasks(workspace scene)
-    public List<string> Sphere; //same as above, name of the lisr = name of the GameObject
-    public List<string> Cylinder;
-    public List<string> Capsule;
+    // public List<string> Cube; //contain all the requirements(reception scene), reference(warehouse scene), tasks(workspace scene)
+    // public List<string> Sphere; //same as above, name of the lisr = name of the GameObject
+    // public List<string> Cylinder;
+    // public List<string> Capsule;
 
     int CharSerial;
     int CRSerial;
@@ -50,45 +50,8 @@ public class GameManager : MonoBehaviour
 
     public void Start()
     {
-        Cube.Add("A");
-        Cube.Add("B");
-        Cube.Add("C");
-        Cube.Add("D");
-        Cube.Add("E");
-        Cube.Add("F");
 
-        Sphere.Add("1");
-        Sphere.Add("2");
-        Sphere.Add("3");
-        Sphere.Add("4");
-        Sphere.Add("5");
-        Sphere.Add("6");
-
-        Cylinder.Add("a");
-        Cylinder.Add("b");
-        Cylinder.Add("c");
-        Cylinder.Add("d");
-        Cylinder.Add("e");
-        Cylinder.Add("f");
-
-        Capsule.Add("aa");
-        Capsule.Add("bb");
-        Capsule.Add("cc");
-        Capsule.Add("dd");
-        Capsule.Add("ee");
-        Capsule.Add("ff");
-
-        // for (int i = 0; i < Cube.Count; i++)
-        // {
-        //     Debug.Log(Cube[i]);
-        // }
     }
-
-
-
-
-
-
 
     void Awake()
     {
@@ -105,7 +68,7 @@ public class GameManager : MonoBehaviour
     public void RandomChar()
     {
         RandomCharacter[CharSerial].SetActive(false); //hide original characters
-        CharSerial = Random.Range(0, 4);
+        CharSerial = Random.Range(0, 5);
         Debug.Log("CharSerial=" + CharSerial);
         RandomCharacter[CharSerial].SetActive(true); //show new characters
 
